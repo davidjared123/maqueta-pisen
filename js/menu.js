1de271640a8cb14256e90b254278a4960a2ca4bf
davidjared123 <52287908+davidjared123@users.noreply.github.com>
2020-12-01 00:12:05 -0400

    añadido efecto js boton float

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,19 @@
+    // funcion para activar menu responsive
 function actived(){
     document.getElementById('nav').classList.toggle('navbar-links-responsive')
+    document.getElementById('nav').classList.toggle('navbar-links')
+}
+    // Funcion para efecto de mostrar btn 
+function onScrollTop(){
+    if (window.pageYOffset > 350 ) {
+        document.querySelector(".float").style.bottom = "15px";
+    } else {
+        document.querySelector(".float").style.bottom = "-45px";
+    }
+}
+
+    // funcion para btn scroll Up
+function scrollUp(){
+     window.scroll(0,0)
 }
 
